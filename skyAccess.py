7affb304594a425d32e5bd56239ec97fc82e6c2c
Farhang Habibi <17721302+farhangh@users.noreply.github.com>
2018-12-12 11:46:38 +0100

    Update skyAccess.py

--- skyAccess.py
+++ skyAccess.py
@@ -2,6 +2,9 @@
 This code plots the sky visibility in declination and hour angle for a
 given telescope and observation time (here is the JKT telescope).
 The contours show the accesible coordinates with constant zenith angles.
+
+December 2018 - Azar 1397
+farhang@nailydata.com
 '''
 
 import numpy as np
